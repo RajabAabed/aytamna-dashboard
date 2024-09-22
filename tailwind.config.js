9ad29a3b9a64f77d3edf45dbf91eb8e537e29e3f
Rajab M. Aabed <rajab7abed@gmail.com>
2024-09-22 14:05:23 +0300

    setup

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,30 @@
 module.exports = {
   content: ["./dist/**/*.{html,js}"],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        IBM: ["IBM Plex Sans Arabic", "sans-serif"],
+      },
+      color: {
+        primary: {
+          100: "#BAC2CB",
+          500: "#223B58",
+          600: " #1F354F",
+          700: "#1B2F46",
+        },
+      },
+      fontSize: {
+        h1: "42px",
+        h2: "38px",
+        h3: "32px",
+        h4: "26px",
+        title: "22px",
+        subTitle: "18px",
+        body: "16px",
+        description: "14px",
+        badges: "12px",
+      },
+    },
   },
-  plugins: [],
+  plugins: [require("@tailwindcss/forms")],
 };
